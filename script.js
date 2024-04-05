84376fbad1867cf3e2c760878a74a8106e676206
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2024-04-05 10:07:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll("section a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].setAttribute("target", "_blank");
+  }
+});
+
+//Simula SPA
+document.addEventListener("DOMContentLoaded", function () {
+  const views = document.querySelectorAll(".view");
+  const viewShow = (id) => {
+    views.forEach((view) => (view.style.display = "none"));
+    document.getElementById(id).style.display = "block";
+  };
+
+  //Enrutador
+  document.getElementById("viewlink1").addEventListener("click", function () {
+    viewShow("view1");
+  });
+
+  document.getElementById("viewlink2").addEventListener("click", function () {
+    viewShow("view2");
+  });
+
+  document.getElementById("viewlink3").addEventListener("click", function () {
+    viewShow("view3");
+  });
+  document.getElementById("viewlink4").addEventListener("click", function () {
+    viewShow("view4");
+  });
+
+  viewShow("view1");
+});
